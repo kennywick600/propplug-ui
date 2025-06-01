8f77b3e8cb87703213d2f1801506252e433b660e
Franko <ocanzan37@gmail.com>
2025-06-01 00:25:54 -0400

    ✅ Manually added Tailwind and PostCSS config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 ﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {},
